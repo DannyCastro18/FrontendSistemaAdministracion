5eefdc6d0565d795579c42177495b26229f72929
Danny Castro <danllelacrespactr018@gmail.com>
2025-02-19 10:01:16 -0500

    Frontend

---
+++ app/components/navbar.jsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from 'next/link';
+
+const NavbarAdmin = () => {
+    return (
+        <aside className='w-16 mr-3 p-1.5 bg-white/50 h-[96vh] top-0 flex justify-center rounded '>
+            <nav className='flex flex-col justify-bettween items-center w-full rounded bg-slate-50 group-hover:items-baseline group-hover:pl-4'>
+                <ul className='flex gap-2 flex-col justify-center py-2'>
+
+                </ul>
+            </nav>
+        </aside>
+    )
+    
+}
+
+export default NavbarAdmin;
