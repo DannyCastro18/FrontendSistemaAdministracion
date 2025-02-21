7196c282ee04d268e5bbe5da917a6901958496bd
DannyCastro18 <danllelacrespactr018@gmail.com>
2025-02-21 07:49:32 -0500

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import NavbarAdmin from "./components/Navbar";
 import BotonAgregar from "./components/BotonAgregar";
+import Header from "./components/Header";
 
 export default function dashboard() {
   return (
     <div className="">
+      <Header />
       <NavbarAdmin />
       <BotonAgregar />
     </div>
