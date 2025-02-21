620ad60de6d79ca1e93ee6356db92b1a4dcf37eb
Danny Castro <danllelacrespactr018@gmail.com>
2025-02-21 07:09:47 -0500

    Nuevos componentes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import NavbarAdmin from "./components/Navbar";
 import BotonAgregar from "./components/BotonAgregar";
+import TableOwner from "./components/TableOwner";
 
 export default function dashboard() {
   return (
     <div className="">
       <NavbarAdmin />
       <BotonAgregar />
+      <TableOwner/>
     </div>
   );
 }
