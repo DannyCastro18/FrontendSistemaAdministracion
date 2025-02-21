45df7bb4f3b821922929fe9db7e71e20fcbe5c0d
DannyCastro18 <danllelacrespactr018@gmail.com>
2025-02-21 10:57:35 -0500

    rutas de admin

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,8 +1,8 @@
-import TableAdmin from "@/app/components/TableAdmin";
+import TableOwner from "@/app/components/TableOwner";
 export default function DashboardPage() {
     return (
         <div className='flex flex-col w-full h-full'>
-            <TableAdmin />
+            <TableOwner />
         </div>
     );
 }
---
+++ app/admin/payments/page.tsx
@@ -0,0 +1,10 @@
+import TablePayments from "@/app/components/TablePayments";
+
+export default function PaymentsPage() {
+    return (
+        <>
+        <h1>Payments</h1>
+            <TablePayments />
+        </>
+    );
+}
