041846ac70b060b8c9de6a489b43a6c6664206d9
DannyCastro18 <danllelacrespactr018@gmail.com>
2025-02-21 11:42:28 -0500

    conexion propietarios frontend

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,8 +1,8 @@
-import TableOwner from "@/app/components/TableOwner";
+import TableAdmin from "@/app/components/TableAdmin";
 export default function DashboardPage() {
     return (
         <div className='flex flex-col w-full h-full'>
-            <TableOwner />
+            <TableAdmin />
         </div>
     );
 }
